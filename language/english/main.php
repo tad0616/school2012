f5ebe56e2cf36bf01a277b4cf4eaa880a31dc628
Tad <tad0616@gmail.com>
2014-05-06 16:44:19 +0800

    加入左右區域分隔線的額外設定

--- language/english/main.php
+++ language/english/main.php
@@ -39,4 +39,11 @@ define("TF_USE_SHADOW_DESC", "");
 
 define("TF_USE_SHADOW_LOGO", " Display logo shadow?");
 define("TF_USE_SHADOW_LOGO_DESC", "");
+
+define('TF_LEFT_SPARATE','Left separate line');
+define('TF_LEFT_SPARATE_DESC','Display separate line between left block zone and main content?');
+define('TF_RIGHT_SPARATE','Right separate line');
+define('TF_RIGHT_SPARATE_DESC','Display separate line between main content and right block zone?');
+define('TF_SPARATE_STYLE','Separate line style');
+define('TF_SPARATE_STYLE_DESC','Setup separate line style.');
 ?>
--- language/tchinese/main.php
+++ language/tchinese/main.php
@@ -39,4 +39,11 @@ define("TF_USE_SHADOW_DESC", "若選是，外框會出現陰影");
 
 define("TF_USE_SHADOW_LOGO", "陰影是否包含logo");
 define("TF_USE_SHADOW_LOGO_DESC", "當logo在滑動圖文之上時，是否仍要加上陰影？");
+
+define('TF_LEFT_SPARATE','左區域分隔線');
+define('TF_LEFT_SPARATE_DESC','左區域和主內容間是否加上分隔線');
+define('TF_RIGHT_SPARATE','右區域分隔線');
+define('TF_RIGHT_SPARATE_DESC','右區域和主內容間是否加上分隔線');
+define('TF_SPARATE_STYLE','左右區域分隔線的樣式');
+define('TF_SPARATE_STYLE_DESC','左右區域分隔線的樣式');
 ?>
